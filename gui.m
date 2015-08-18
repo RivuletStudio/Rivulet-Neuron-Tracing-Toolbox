0d36d3cbc1f30573e8a9df5030197b53dcf42bf6
Siqi Liu <lsqshr@gmail.com>
2015-08-18 11:59:36 +1000

    gui draft

--- gui.m
+++ gui.m
@@ -22,7 +22,7 @@ function varargout = gui(varargin)
 
 % Edit the above text to modify the response to help gui
 
-% Last Modified by GUIDE v2.5 18-Aug-2015 10:54:54
+% Last Modified by GUIDE v2.5 18-Aug-2015 11:44:37
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -169,3 +169,102 @@ function checkbox3_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 % Hint: get(hObject,'Value') returns toggle state of checkbox3
+
+
+
+function edit3_Callback(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit3 as text
+%        str2double(get(hObject,'String')) returns contents of edit3 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit3_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton4.
+function pushbutton4_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+function edit4_Callback(hObject, eventdata, handles)
+% hObject    handle to edit4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit4 as text
+%        str2double(get(hObject,'String')) returns contents of edit4 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit4_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit5_Callback(hObject, eventdata, handles)
+% hObject    handle to edit5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit5 as text
+%        str2double(get(hObject,'String')) returns contents of edit5 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit5_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in checkbox4.
+function checkbox4_Callback(hObject, eventdata, handles)
+% hObject    handle to checkbox4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of checkbox4
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
--- trytip3d.m
+++ trytip3d.m
@@ -1,127 +1,153 @@
-% function trytip3d(I)
+imgpath = '/home/siqi/hpc-data1/Data/first2000/first2000-subsets/first50/sorted/8.v3draw';
+segmentmethod = 'threshold';
+threshold = 0;
+plot = true;
+delta_t = 0.5;
+percentage = 0.98;
+crop = true;
+rewire = false;
+
+
+[pathstr, ~, ~] = fileparts(mfilename('fullpath'));
+addpath(fullfile(pathstr, '..', '..', 'v3d', 'v3d_external', 'matlab_io_basicdatatype'));
+addpath(fullfile(pathstr, 'util'));
+addpath(genpath(fullfile(pathstr, 'lib')));
+
+disp('Loading Image and segment foreground...');
+% I = load_v3d_raw_img_file(imgpath);
+
 tic;
-clear;
-	disp('Loading Image and classifying...')
-	clsf = load('/home/siqi/hpc-data1/Data/OP/quad.mat');
-    cl = clsf.obj;
-	[I] = binarizeimage('threshold', '/home/siqi/hpc-data1/Data/first2000/first2000-subsets/first5/00001.FruMARCM-M002262_seg001.lsm.tif.c3.v3draw.uint8.v3draw', 0, 1, true);
+fprintf('Segmenting image using %s\n', segmentmethod);
+if strcmp(segmentmethod, 'threshold')
+    [I, cropregion] = binarizeimage(segmentmethod, imgpath, threshold, delta_t, crop);
+else
+    [I, cropregion] = binarizeimage(segmentmethod, imgpath, cl, delta_t, crop);
+end
 
-    disp('Distance transform');
-    bdist = getBoundaryDistance(I, true);
-    disp('Looking for the source point...')
-    [SourcePoint, maxD] = maxDistancePoint(bdist, I, true);
-    disp('Make the speed image...')
-    SpeedImage=(bdist/maxD).^4;
-	SpeedImage(SpeedImage==0) = 1e-10;
-	disp('marching...');
-    oT = msfm(SpeedImage, SourcePoint, false, false);
-    disp('Finish marching')
 
-    disp('Calculating gradient...')
-	% Calculate gradient of DistanceMap
+disp('Distance transform');
+bdist = getBoundaryDistance(I, true);
+disp('Looking for the source point...')
+[SourcePoint, maxD] = maxDistancePoint(bdist, I, true);
+disp('Make the speed image...')
+SpeedImage=(bdist/maxD).^4;
+SpeedImage(SpeedImage==0) = 1e-10;
+disp('marching...');
+oT = msfm(SpeedImage, SourcePoint, false, false);
+disp('Finish marching')
 
-    close all
-    T = oT;
-    tree = []; % swc tree
-    prune = true;
-	grad = distgradient(T);
-    S = {};
-    B = zeros(size(T));
-    i = 1;
-	figure(1)
-	showbox(I, 0.5);
-	drawnow
+disp('Calculating gradient...')
+% Calculate gradient of DistanceMap
 
-	% figure(2)
-	% showbox(I, 0.5);
-	% drawnow
+close all
+T = oT;
+tree = []; % swc tree
+prune = true;
+grad = distgradient(T);
+S = {};
+B = zeros(size(T));
+i = 1;
+figure(1)
+showbox(I, 0.5);
+drawnow
 
-    lconfidence = [];
+lconfidence = [];
+if plot
     hold on
     [x,y,z] = sphere;
     surf(x + SourcePoint(2), y + SourcePoint(1), z + SourcePoint(3));
+end
 
-    while(true)
+while(true)
 
-	    StartPoint = maxDistancePoint(T, I, true);
+    StartPoint = maxDistancePoint(T, I, true);
+    if plot
 	    surf(x + StartPoint(2), y + StartPoint(1), z + StartPoint(3));
+	end
 
-	    % disp('SourcePoint')
-	    % disp(SourcePoint)
-	    % disp('StartPoint')
-	    % disp(StartPoint)
-	    % disp('T:')
-	    % disp(T(StartPoint(1), StartPoint(2), StartPoint(3)))
-	    % disp('B:')
-	    % disp(B(StartPoint(1), StartPoint(2) ,StartPoint(3)))
-	    % disp('I:')
-	    % disp(I(StartPoint(1), StartPoint(2) ,StartPoint(3)))
-
-	    if T(StartPoint(1), StartPoint(2), StartPoint(3)) == 0 || I(StartPoint(1), StartPoint(2), StartPoint(3)) == 0
-	    	break;
-	    end
-
-	    disp('start tracing');
-	    l = shortestpath2(T, grad, StartPoint, SourcePoint, 1, 'rk4');
-	    disp('end tracing')
-
-	    % Get radius of each point from distance transform
-	    ind = sub2ind(size(bdist), int16(l(:, 1)), int16(l(:, 2)), int16(l(:, 3)));
-	    radius = bdist(ind);
-	    radius(radius < 1) = 2;
-	    radius = ceil(radius);
-
-    	% disp('found shorline with length')
-    	% disp(size(l, 1))
-	    if size(l, 1) < 4
-	    	l = [StartPoint'; l];
-	    	radius = zeros(size(l, 1), 1);
-	    	radius(:) = 2;
-	    end
-		[rlistlength, useless] = size(l);
-	    radiuslist = zeros(rlistlength, 1);
-	    for radius_i = 1 : rlistlength
-	    	curradius = getradius(I, l(radius_i, 1), l(radius_i, 2), l(radius_i, 3));
-	    	radiuslist(radius_i) = curradius; 
-	    end 
-	    % Remove the traced path from the timemap
-	    tB = binarysphere3d(size(T), l, radiuslist);
-	    tB(StartPoint(1), StartPoint(2), StartPoint(3)) = 3;
-	    T(tB==1) = -1;
-
-	    % Add l to the tree
-	    if prune && size(l, 1) > 4
-		    [tree, confidence] = addbranch2tree(tree, l, radius, I);
-
-		    if confidence > 0.5
-		    	lconfidence = [lconfidence; confidence];
-			    S{i} = l;
-			    i = i + 1;
-		    end
-		end
-
-        B = B | tB;
-
-        percent = sum(B(:) & I(:)) / sum(I(:))
-        if percent > 0.7
-        	break;
-        end
-
-         figure(2)
-        % % scatter3(B(:, 1), B(:, 2), B(:, 3));
-         showbox(B, 0.5);
-         drawnow
-
+    if T(StartPoint(1), StartPoint(2), StartPoint(3)) == 0 || I(StartPoint(1), StartPoint(2), StartPoint(3)) == 0
+    	break;
     end
-    hold off
 
+    disp('start tracing');
+    l = shortestpath2(T, grad, StartPoint, SourcePoint, 1, 'rk4');
+    disp('end tracing')
+
+    % Get radius of each point from distance transform
+    ind = sub2ind(size(bdist), int16(l(:, 1)), int16(l(:, 2)), int16(l(:, 3)));
+    radius = bdist(ind);
+    radius(radius < 1) = 1;
+    radius = ceil(radius);
+ %    radius = zeros(size(l, 1), 1);
+ %    for r = 1 : size(l, 1)
+	%     radius(r) = getradius(I, l(r, 1), l(r, 2), l(r, 3));
+	% end
+ %    radius(radius < 1) = 1;
+
+    disp('size l')
+    disp(size(l, 1))
+    disp('size radius')
+    disp(size(radius))
+
+    if size(l, 1) < 4
+    	l = [StartPoint'; l];
+    	radius = zeros(size(l, 1), 1);
+    	radius(:) = 2;
+    end
+	[rlistlength, useless] = size(l);
+    radiuslist = zeros(rlistlength, 1);
+    for radius_i = 1 : rlistlength
+    	curradius = getradius(I, l(radius_i, 1), l(radius_i, 2), l(radius_i, 3));
+    	radiuslist(radius_i) = curradius; 
+    end 
+
+    % Remove the traced path from the timemap
+    tB = binarysphere3d(size(T), l, radiuslist);
+    tB(StartPoint(1), StartPoint(2), StartPoint(3)) = 3;
+    T(tB==1) = -1;
+
+    % Add l to the tree
+    if prune && size(l, 1) > 4
+	    [tree, confidence] = addbranch2tree(tree, l, radius, I);
+
+	    if confidence > 0.5 % skip noise points
+	    	lconfidence = [lconfidence; confidence];
+		    S{i} = l;
+		    i = i + 1;
+	    end
+	end
+
+    B = B | tB;
+
+    percent = sum(B(:) & I(:)) / sum(I(:))
+    if percent >= percentage
+    	break;
+    end
+
+end
+hold off
+toc;
+
+% showswc(tree, I, true);
+% showswc(rewiredtree, I, true);
+
+% tree(:, 6) = 1;
+for i = 1 : size(tree, 1)
+    tree(i, 6) = getradius(I, tree(i, 3), tree(i, 4), tree(i, 5));
+end
+
+% Shift the result tree back to the original space if crop was conducted
+if crop
+    tree(:, 3) = tree(:, 3) + cropregion(1, 1);
+    tree(:, 4) = tree(:, 4) + cropregion(2, 1);
+    tree(:, 5) = tree(:, 5) + cropregion(3, 1);
+end
+
+save_v3d_swc_file(tree, [imgpath, '.trace.swc']);
+
+if rewire
     rewiredtree = rewiretree(tree, S, I, lconfidence, 0.7);
-    % showswc(tree, I, true);
-%     showswc(rewiredtree, I, true);
     rewiredtree(:, 6) = 1;
-    tree(:, 6) = 1;
-    
-    save_v3d_swc_file(tree, 'shit.swc');
-    save_v3d_swc_file(rewiredtree, 'rewired-shit.swc');
-toc
-
+    save_v3d_swc_file(rewiredtree, [imgpath, '.rewired.swc']);
+    tree = rewiredtree;
+end
