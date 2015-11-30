fb067b6235d3e4183c5b2506bcce58ad1c9911e7
donghaozhang <dzha9516@uni.sydney.edu.au>
2015-11-30 16:15:41 +1100

    3d quick soma initialization is implemented

--- test/somaini_v_quick.m
+++ test/somaini_v_quick.m
@@ -1,10 +1,27 @@
+clc
+clear all;
+close all;
+
 % function soma = somaini_v_quick(imgsoma, somathres)
+	imgthres = 30;
+	somathres = 70;
 	imgsoma = load_v3d_raw_img_file('/home/donghao/Desktop/smallsoma.v3draw');
 	% somadrthres is the threshold on the image which is perform with direntional ratio tranform
-	safeshowbox(imgsoma, 30)
+	safeshowbox(imgsoma, imgthres)
 	figure
 	soma = imgsoma > somathres;
-	safeshowbox(soma, somathres)
-
+	safeshowbox(soma, 0.5)
+	region = regionprops(soma, 'all');
+	[~, maxareaindex] = max([region.Area]);
+	pasted = zeros(size(soma));
+	maxxyregion = region(maxareaindex);
+	maxxyregionBoundingBox = maxxyregion.BoundingBox;
+	[xwidth, yheight, zdim] = size(region(maxareaindex).FilledImage);
+	xp = maxxyregionBoundingBox(2);
+	yp = maxxyregionBoundingBox(1);
+	zp = maxxyregionBoundingBox(3); 
+	pasted(floor(xp): floor(xp) + xwidth - 1, floor(yp) : floor(yp) + yheight - 1,  floor(zp) : floor(zp) + zdim - 1) = region(maxareaindex).FilledImage;
+	figure
+	safeshowbox(pasted, 0.5)
 
 % end
