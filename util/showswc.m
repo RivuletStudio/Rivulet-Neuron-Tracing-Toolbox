3ccd0fcf3b15cf5486b49b373b34b65336a74661
siqi <lsqshr@gmail.com>
2015-12-24 15:35:33 +1100

    changed swc to red line

--- util/showswc.m
+++ util/showswc.m
@@ -8,7 +8,7 @@ endplot = true;
 if numel(varargin) >= 2
 	endplot = varargin{2};
 end
-
+whitebg(gcf, 'black')
 colormap([0.7 0.7 1]);
 
 hold on
@@ -17,8 +17,11 @@ for i = 1 : size(tree, 1)
     cnode = tree(i, 3:5);
     parent = tree(i, 7);
     [pidx] = find(tree(:, 1) == parent);
-    pnode = tree(pidx, 3:5);
-    plot3([cnode(3);pnode(3)], [cnode(4);pnode(4)], [cnode(5); pnode(5)]);    
+    if numel(pidx) == 1
+        pnode = tree(pidx, 3:5);
+        plot3([cnode(1);pnode(1)], [cnode(2);pnode(2)], [cnode(3); pnode(3)],...
+              'Color', [0.8 0 0]);
+    end
 end
 %The following line  of code can adjust axis ratio
 daspect([1 1 1])
