56628c53085b71335a0bf4ae9c3f29e3eed2b5ff
donghaozhang <dzha9516@uni.sydney.edu.au>
2016-03-16 16:41:43 +1100

    math sequence version of somafield

---
+++ scripts/gaussian3.m
@@ -0,0 +1,46 @@
+%% 3D Gaussian
+function g = gaussian3(s, sigma)
+
+if isempty(s)
+    s = 5;
+end
+
+if isempty(sigma)
+    sigma = s/(4*sqrt(2*log(2)));
+end
+
+super_s = ceil(sigma * (4*sqrt(2*log(2))));
+
+if super_s > s
+    super_g = zeros(super_s, super_s, super_s);
+    c = (1+super_s)/2;
+    for x = 1:super_s
+        for y = 1:super_s
+            for z = 1:super_s
+                r = (x-c)^2 + (y-c)^2 + (z-c)^2;
+                super_g(x, y, z) = exp(-(2*sigma^2)/r);
+                % super_g(x, y, z) = log(r/(2*sigma^2));
+            end
+        end
+    end
+    super_g = super_g/sum(super_g(:));
+    m = ceil((super_s - s)/2);
+    n = floor((super_s - s)/2);
+    g = super_g(m:end-n, m:end-n, m:end-n);
+else
+    g = zeros(s, s, s);
+    c = (1+s)/2;
+    for x = 1:s
+        for y = 1:s
+            for z = 1:s
+                r = (x-c)^2 + (y-c)^2 + (z-c)^2;
+                % g(x, y, z) = exp(-r/(2*sigma^2));
+                g(x, y, z) = exp(-(2*sigma^2)/r);
+            end
+        end
+    end
+    g = g/sum(g(:));
+end
+
+
+end
--- trace.m
+++ trace.m
@@ -10,32 +10,32 @@ function [tree, meanconf] = trace(varargin)
 % crop(optional): crop the image with threshold > 0; default true
 % rewire: whether the result tree will be rewired
 
-	I = varargin{1};
+    I = varargin{1};
 
-	plot = false;
+    plot = false;
     if numel(varargin) >= 2
-		plot = varargin{2};
-	end
+        plot = varargin{2};
+    end
 
-	percentage = 0.95;
+    percentage = 0.95;
     if numel(varargin) >= 3
-		percentage = double(varargin{3});
-	end
+        percentage = double(varargin{3});
+    end
 
-	rewire = false;
-	if numel(varargin) >= 4
-		rewire = varargin{4};
+    rewire = false;
+    if numel(varargin) >= 4
+        rewire = varargin{4};
     end
     
-	gap = 10;
+    gap = 10;
     if numel(varargin) >= 5
-		gap = varargin{5};
-	end
+        gap = varargin{5};
+    end
 
-	ax = false;
+    ax = false;
     if numel(varargin) >= 6
-		ax = varargin{6};
-	end
+        ax = varargin{6};
+    end
 
     dumpcheck = false;
     if numel(varargin) >= 7
@@ -95,7 +95,7 @@ function [tree, meanconf] = trace(varargin)
         bdist = double(bdist);
     end                        
     
-	[pathstr, ~, ~] = fileparts(mfilename('fullpath'));
+    [pathstr, ~, ~] = fileparts(mfilename('fullpath'));
     addpath(fullfile(pathstr, 'util'));
     addpath(genpath(fullfile(pathstr, 'lib')));
 
@@ -119,18 +119,49 @@ function [tree, meanconf] = trace(varargin)
         % Find the soma radius
         d = pdist2([somax, somay, somaz], [soma.x, soma.y, soma.z]);
         maxD = max(d);
+        fprintf('bug one?\n'); 
+        centersoma = zeros(size(I));
+        centersoma(round(SourcePoint(1)), round(SourcePoint(2)), round(SourcePoint(3))) = 1;
+        T_soma_center = bwdist(centersoma > 0.5, 'Quasi-Euclidean');
+        scale_k = 2;
+        T_soma_center = T_soma_center.^(1/scale_k);
+        scale_k2 = 600000;
+        T_soma_center = T_soma_center * scale_k2;
+        T_soma_center = T_soma_center.*double((I>0.5));
+        fprintf('bug two?\n'); 
+        % somaSourcepoint = [somax, somay, somaz];
+        % sizesomaSourcepoint = size(somaSourcepoint); 
+        % fprintf('The first dimension of source point is : %d\n', sizesomaSourcepoint(1));
+        % fprintf('The second dimension of source point is : %d\n', sizesomaSourcepoint(2));
     else
         [SourcePoint, maxD] = maxDistancePoint(bdist, I, true);
+        % fprintf('I want to see this line first');
+        % disp(SourcePoint);
+        % fprintf('The size of source point is : %d', size(SourcePoint));
+
     end
     disp('Make the speed image...')
     SpeedImage=(bdist/maxD).^4;
     clear bdist;
     SpeedImage(SpeedImage==0) = 1e-10;
-	if plot
+    if plot
         axes(ax);
-	end	
-	disp('marching...');
+    end 
+    disp('marching...');
     T = msfm(SpeedImage, SourcePoint, false, false);
+    T = T + double(T_soma_center);
+    % notsomabI = not(soma.I<0.5);
+    % T_soma = bwdist(notsomabI, 'Quasi-Euclidean');
+    % T_soma = T_soma + soma.I; 
+    % T_soma = double(T_soma);
+    % T_soma = 1./ T_soma;
+    % T_soma = T_soma / min(T_soma(:));
+    % T_soma = T_soma.*double((I>0.5));
+    % T_soma_alpha = 7000;
+    % T_beta = 70;
+    save('SpeedImage');
+    
+    % T = T / T_beta + T_soma_alpha * T_soma; 
     szT = size(T);
     fprintf('the size of time map, x is : %d, y is : %d, z is : %d\n', szT(1), szT(2), szT(3));
     disp('Finish marching')
@@ -141,7 +172,7 @@ function [tree, meanconf] = trace(varargin)
     end
 
     if plot
-    	hold on 
+        hold on 
     end
 
     tree = []; % swc tree
@@ -158,8 +189,8 @@ function [tree, meanconf] = trace(varargin)
     end
 
     prune = true;
-	% Calculate gradient of DistanceMap
-	disp('Calculating gradient...')
+    % Calculate gradient of DistanceMap
+    disp('Calculating gradient...')
     grad = distgradient(T);
     if plot
         axes(ax);
@@ -171,40 +202,40 @@ function [tree, meanconf] = trace(varargin)
     end
     lconfidence = [];
     if plot
-	    [x,y,z] = sphere;
-	    plot3(x + SourcePoint(2), y + SourcePoint(1), z + SourcePoint(3), 'ro');
-	end
+        [x,y,z] = sphere;
+        plot3(x + SourcePoint(2), y + SourcePoint(1), z + SourcePoint(3), 'ro');
+    end
 
     unconnectedBranches = {};
     printcount = 0;
     printn = 0;
     while(true)
 
-	    StartPoint = maxDistancePoint(T, I, true);
-	    if plot
-		    plot3(x + StartPoint(2), y + StartPoint(1), z + StartPoint(3), 'ro');
-		end
+        StartPoint = maxDistancePoint(T, I, true);
+        if plot
+            plot3(x + StartPoint(2), y + StartPoint(1), z + StartPoint(3), 'ro');
+        end
 
-	    if T(StartPoint(1), StartPoint(2), StartPoint(3)) == 0 || I(StartPoint(1), StartPoint(2), StartPoint(3)) == 0
-	    	break;
-	    end
+        if T(StartPoint(1), StartPoint(2), StartPoint(3)) == 0 || I(StartPoint(1), StartPoint(2), StartPoint(3)) == 0
+            break;
+        end
 
-	    [l, dump, merged, somamerged] = shortestpath2(T, grad, I, tree, StartPoint, SourcePoint, 1, 'rk4', gap);
+        [l, dump, merged, somamerged] = shortestpath2(T, grad, I, tree, StartPoint, SourcePoint, 1, 'rk4', gap);
 
         if size(l, 1) == 0
             l = StartPoint'; % Make sure the start point will be erased
         end
         
-	    % Get radius of each point from distance transform
-	    radius = zeros(size(l, 1), 1);
-	    parfor r = 1 : size(l, 1)
-		    radius(r) = getradius(I, l(r, 1), l(r, 2), l(r, 3));
-		end
-	    radius(radius < 1) = 1;
-		assert(size(l, 1) == size(radius, 1));
+        % Get radius of each point from distance transform
+        radius = zeros(size(l, 1), 1);
+        parfor r = 1 : size(l, 1)
+            radius(r) = getradius(I, l(r, 1), l(r, 2), l(r, 3));
+        end
+        radius(radius < 1) = 1;
+        assert(size(l, 1) == size(radius, 1));
 
         [covermask, centremask] = binarysphere3d(size(T), l, radius);
-	    % Remove the traced path from the timemap
+        % Remove the traced path from the timemap
         if cleanercheck & size(l, 1) > branchlen
             covermask = augmask(covermask, I, l, radius);
         end
@@ -214,15 +245,15 @@ function [tree, meanconf] = trace(varargin)
         T(covermask) = -1;
         T(centremask) = -3;
 
-	    % if cleanercheck
+        % if cleanercheck
      %        T(wash==1) = -1;
      %    end
 
-	    % Add l to the tree
-	    if ~((dump) && dumpcheck) 
-		    [tree, newtree, conf, unconnected] = addbranch2tree(tree, l, merged, connectrate, radius, I, branchlen, plot, somamerged);
+        % Add l to the tree
+        if ~((dump) && dumpcheck) 
+            [tree, newtree, conf, unconnected] = addbranch2tree(tree, l, merged, connectrate, radius, I, branchlen, plot, somamerged);
             lconfidence = [lconfidence, conf];
-		end
+        end
 
         B = B | covermask;
 
@@ -236,8 +267,8 @@ function [tree, meanconf] = trace(varargin)
             printcount = fprintf('Tracing percent: %f%%\n', percent*100);
         end
         if percent >= percentage
-        	disp('Coverage reached end tracing...')
-        	break;
+            disp('Coverage reached end tracing...')
+            break;
         end
 
     end
@@ -250,8 +281,8 @@ function [tree, meanconf] = trace(varargin)
     end 
     tree = prunetree(tree, branchlen);
 
-	if plot
-		hold off
-	end
+    if plot
+        hold off
+    end
 
 end
