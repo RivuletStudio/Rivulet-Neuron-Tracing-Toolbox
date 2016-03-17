06e3badb3e7dd7170a217b72aa6eb4d5c4d32d7f
siqi <lsqshr@gmail.com>
2016-03-17 12:33:31 +1100

    deleted some redundent code for 2D to make things slightly faster

--- lib/FastMarching_version3b/shortestpath/rk4.c
+++ lib/FastMarching_version3b/shortestpath/rk4.c
@@ -16,62 +16,14 @@
  * Function is written by D.Kroon University of Twente (July 2008)
  */
 
-__inline int mindex2(int x, int y, int sizx)  { return y*sizx+x;}
 __inline int mindex3(int x, int y, int z, int sizx, int sizy)  { return z*sizy*sizx+y*sizx+x;}
-
-
-__inline int checkBounds2d( double *point, int *Isize) {
-    if((point[0]<0)||(point[1]<0)||(point[0]>(Isize[0]-1))||(point[1]>(Isize[1]-1))) { return false; }
-    return true;
-}
 
 __inline int checkBounds3d( double *point, int *Isize) {
     if((point[0]<0)||(point[1]<0)||(point[2]<0)||(point[0]>(Isize[0]-1))||(point[1]>(Isize[1]-1))||(point[2]>(Isize[2]-1))) { return false; }
     return true;
 }
 
-__inline double norm2(double *a) { return sqrt(a[0]*a[0]+a[1]*a[1]); }
 __inline double norm3(double *a) { return sqrt(a[0]*a[0]+a[1]*a[1]+a[2]*a[2]); }
-
-
-__inline void interpgrad2d(double *Ireturn, double *I, int *Isize, double *point) {
-    /*  Linear interpolation variables */
-    int xBas0, xBas1, yBas0, yBas1;
-    double perc[4]={0, 0, 0, 0};
-    double xCom, yCom, xComi, yComi;
-    double fTlocalx, fTlocaly;
-    int f;
-    int index[4];
-    
-    fTlocalx = floor(point[0]); fTlocaly = floor(point[1]);
-    xBas0=(int) fTlocalx; yBas0=(int) fTlocaly;
-    xBas1=xBas0+1; yBas1=yBas0+1;
-    
-    /* Linear interpolation constants (percentages) */
-    xCom=point[0]-fTlocalx; yCom=point[1]-fTlocaly;
-    xComi=(1-xCom); yComi=(1-yCom);
-    perc[0]=xComi * yComi;
-    perc[1]=xComi * yCom;
-    perc[2]=xCom * yComi;
-    perc[3]=xCom * yCom;
-    
-    /* Stick to boundary */
-    if(xBas0<0) { xBas0=0; if(xBas1<0) { xBas1=0; }}
-    if(yBas0<0) { yBas0=0; if(yBas1<0) { yBas1=0; }}
-    if(xBas1>(Isize[0]-1)) { xBas1=Isize[0]-1; if(xBas0>(Isize[0]-1)) { xBas0=Isize[0]-1; }}
-    if(yBas1>(Isize[1]-1)) { yBas1=Isize[1]-1; if(yBas0>(Isize[1]-1)) { yBas0=Isize[1]-1; }}
-    
-    /* Get the neighbour intensities */
-    index[0]=mindex2(xBas0, yBas0, Isize[0]);
-    index[1]=mindex2(xBas0, yBas1, Isize[0]);
-    index[2]=mindex2(xBas1, yBas0, Isize[0]);
-    index[3]=mindex2(xBas1, yBas1, Isize[0]);
-    f=Isize[0]*Isize[1];
-    
-    /* the interpolated color */
-    Ireturn[0]=I[index[0]]*perc[0]+I[index[1]]*perc[1]+I[index[2]]*perc[2]+I[index[3]]*perc[3];
-    Ireturn[1]=I[index[0]+f]*perc[0]+I[index[1]+f]*perc[1]+I[index[2]+f]*perc[2]+I[index[3]+f]*perc[3];
-}
 
 __inline void interpgrad3d(double *Ireturn, double *I, int *Isize, double *point) {
     /*  Linear interpolation variables */
@@ -123,75 +75,6 @@ __inline void interpgrad3d(double *Ireturn, double *I, int *Isize, double *point
     Ireturn[1]=temp+I[index[4]+f0]*perc[4]+I[index[5]+f0]*perc[5]+I[index[6]+f0]*perc[6]+I[index[7]+f0]*perc[7];
     temp=I[index[0]+f1]*perc[0]+I[index[1]+f1]*perc[1]+I[index[2]+f1]*perc[2]+I[index[3]+f1]*perc[3];
     Ireturn[2]=temp+I[index[4]+f1]*perc[4]+I[index[5]+f1]*perc[5]+I[index[6]+f1]*perc[6]+I[index[7]+f1]*perc[7];
-}
- 
-bool RK4STEP_2D(double *gradientArray, int *gradientArraySize, double *startPoint, double *nextPoint, double stepSize) {
-    /* Perform one step of the RK4 algorithm */
-    double k1[2], k2[2], k3[2], k4[2];
-    double tempPoint[2];
-    double tempnorm;
-    
-    
-   /*Calculate k1 */
-    interpgrad2d(k1, gradientArray, gradientArraySize, startPoint);
-    tempnorm=norm2(k1);
-    k1[0] = k1[0]*stepSize/tempnorm;
-    k1[1] = k1[1]*stepSize/tempnorm;
-    
-    tempPoint[0]=startPoint[0] - k1[0]*0.5;
-    tempPoint[1]=startPoint[1] - k1[1]*0.5;
-    
-   /*Check the if are still inside the domain */
-    if (!checkBounds2d(tempPoint, gradientArraySize)) return false;
-    
-   /*Calculate k2 */
-    interpgrad2d(k2, gradientArray, gradientArraySize, tempPoint);
-    tempnorm=norm2(k2);
-    k2[0] = k2[0]*stepSize/tempnorm;
-    k2[1] = k2[1]*stepSize/tempnorm;
-    
-    tempPoint[0]=startPoint[0] - k2[0]*0.5;
-    tempPoint[1]=startPoint[1] - k2[1]*0.5;
-    
-   /*Check the if are still inside the domain */
-    if (!checkBounds2d(tempPoint, gradientArraySize)) return false;
-    
-   /*Calculate k3 */
-    interpgrad2d(k3, gradientArray, gradientArraySize, tempPoint);
-    tempnorm=norm2(k3);
-    k3[0] = k3[0]*stepSize/tempnorm;
-    k3[1] = k3[1]*stepSize/tempnorm;
-    
-    tempPoint[0]=startPoint[0] - k3[0];
-    tempPoint[1]=startPoint[1] - k3[1];
-    
-   /*Check the if are still inside the domain */
-    if (!checkBounds2d(tempPoint, gradientArraySize)) return false;
-    
-   /*Calculate k4 */
-    interpgrad2d(k4, gradientArray, gradientArraySize, tempPoint);
-    tempnorm=norm2(k4);
-    k4[0] = k4[0]*stepSize/tempnorm;
-    k4[1] = k4[1]*stepSize/tempnorm;
-    
-   /*Calculate final point */
-    nextPoint[0] = startPoint[0] - (k1[0] + k2[0]*2.0 + k3[0]*2.0 + k4[0])/6.0;
-    nextPoint[1] = startPoint[1] - (k1[1] + k2[1]*2.0 + k3[1]*2.0 + k4[1])/6.0;
-    
-    /* Set step to step size */
-    /*
-    D[0]=(nextPoint[0]-startPoint[0]);
-    D[1]=(nextPoint[1]-startPoint[1]);
-    dl=stepSize/(sqrt(D[0]*D[0]+D[1]*D[1])+1e-15);
-    D[0]*=dl; D[1]*=dl;
-    nextPoint[0]=startPoint[0]+D[0];
-    nextPoint[1]=startPoint[1]+D[1];
-    */
-    
-   /*Check the if are still inside the domain */
-    if (!checkBounds2d(nextPoint, gradientArraySize)) return false;
-    
-    return true;
 }
 
 
@@ -307,38 +190,19 @@ void mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[] ) {
     nextPoint= mxGetPr(plhs[0]);
     
    /*Perform the RK4 raytracing step */
-    if(PointLength==2) {
-        gradientArraySize[0]=gradientArraySizeC[0];
-        gradientArraySize[1]=gradientArraySizeC[1];
-        startPoint1[0]=startPoint[0]-1.0; 
-        startPoint1[1]=startPoint[1]-1.0;
-        if(RK4STEP_2D(gradientArray, gradientArraySize, startPoint1, nextPoint, stepSize)) {
-            nextPoint[0]=nextPoint[0]+1.0; 
-            nextPoint[1]=nextPoint[1]+1.0;
-        }
-        else {
-            nextPoint[0]=0; nextPoint[1]=0;
-        }
-    }
-    else if(PointLength==3) {
-        gradientArraySize[0]=gradientArraySizeC[0];
-        gradientArraySize[1]=gradientArraySizeC[1];
-        gradientArraySize[2]=gradientArraySizeC[2];
-        /* mexPrintf("RK4 - GradientSize: %d, %d, %d\n", gradientArraySize[0], gradientArraySize[1], gradientArraySize[2]);*/
-        startPoint1[0]=startPoint[0]-1.0; 
-        startPoint1[1]=startPoint[1]-1.0; 
-        startPoint1[2]=startPoint[2]-1.0;
-        if(RK4STEP_3D(gradientArray, gradientArraySize, startPoint1, nextPoint, stepSize)) {
-            nextPoint[0]=nextPoint[0]+1.0; 
-            nextPoint[1]=nextPoint[1]+1.0; 
-            nextPoint[2]=nextPoint[2]+1.0; 
-        }
-        else {
-            nextPoint[0]=0; nextPoint[1]=0; nextPoint[2]=0;
-        }
-        
+    gradientArraySize[0]=gradientArraySizeC[0];
+    gradientArraySize[1]=gradientArraySizeC[1];
+    gradientArraySize[2]=gradientArraySizeC[2];
+    /* mexPrintf("RK4 - GradientSize: %d, %d, %d\n", gradientArraySize[0], gradientArraySize[1], gradientArraySize[2]);*/
+    startPoint1[0]=startPoint[0]-1.0; 
+    startPoint1[1]=startPoint[1]-1.0; 
+    startPoint1[2]=startPoint[2]-1.0;
+    if(RK4STEP_3D(gradientArray, gradientArraySize, startPoint1, nextPoint, stepSize)) {
+        nextPoint[0]=nextPoint[0]+1.0; 
+        nextPoint[1]=nextPoint[1]+1.0; 
+        nextPoint[2]=nextPoint[2]+1.0; 
     }
     else {
-        mexErrMsgTxt("Starting Point must be 2D or 3D");
+        nextPoint[0]=0; nextPoint[1]=0; nextPoint[2]=0;
     }
 }
--- shortestpath2.m
+++ shortestpath2.m
@@ -40,18 +40,10 @@ stepsremain = -1; % It is only used when it touched the surface of any previousl
 
 % Iteratively trace the shortest line
 while(true)
-    if (ndims(T) == 2)
-        disp([ceil(StartPoint(1)), ceil(StartPoint(2))]);
-        if isnan(ceil(StartPoint(1))) || isnan(ceil(StartPoint(2)))
-            break;
-        end
-        dist = T(ceil(StartPoint(1)), ceil(StartPoint(2)));
-    else
-        if isnan(ceil(StartPoint(1))) || isnan(ceil(StartPoint(2))) || isnan(ceil(StartPoint(3)))
-            break;
-        end
-        dist = T(ceil(StartPoint(1)), ceil(StartPoint(2)), ceil(StartPoint(3)));
+    if isnan(ceil(StartPoint(1))) || isnan(ceil(StartPoint(2))) || isnan(ceil(StartPoint(3)))
+        break;
     end
+    dist = T(ceil(StartPoint(1)), ceil(StartPoint(2)), ceil(StartPoint(3)));
     
     if dist == -3 % It reaches the centreline mask
         break;
@@ -92,55 +84,17 @@ while(true)
             error('shortestpath:input','unknown method');
     end
 
-    % scatter3(EndPoint(2), EndPoint(1), EndPoint(3), 'r');
-    % plot3([EndPoint(2);StartPoint(2)], [EndPoint(1);StartPoint(1)], [EndPoint(3); StartPoint(3)], 'b.');
-    % drawnow
-
-    if (ndims(T) == 2)
-        if isnan(ceil(EndPoint(1))) || isnan(ceil(EndPoint(2)))
-            break;
-        end
-    else
-        if isnan(ceil(EndPoint(1))) || isnan(ceil(EndPoint(2))) || isnan(ceil(EndPoint(3)))
-            break;
-        end
+    if isnan(ceil(EndPoint(1))) || isnan(ceil(EndPoint(2))) || isnan(ceil(EndPoint(3)))
+        break;
     end
 
 
     % Calculate the distance to the end point
-    if(~isempty(SourcePoint))
         [DistancetoEnd,ind]=min(sqrt(sum((SourcePoint-repmat(EndPoint,1,size(SourcePoint,2))).^2,1)));
-    else
-        
-        DistancetoEnd=inf;
-    end
     
     % Calculate the movement between current point and point 15 itterations back
     if(i>15), Movement=sqrt(sum((EndPoint(:)-ShortestLine(i-15,:)').^2));  else Movement=Stepsize+1;  end
     
-%     fprintf('I: %f; j: %i\n', I(int16(EndPoint(1)), int16(EndPoint(2)), int16(EndPoint(3))), j);
-    [Ixsize, Iysize, Izsize] = size(I);
-    Ixvalue = int16(EndPoint(1));
-    Iyvalue = int16(EndPoint(2));
-    Izvalue = int16(EndPoint(3));
-    if Ixvalue <= 0
-        Ixvalue =1;
-    elseif Ixvalue >= Ixsize
-        Ixvalue = Ixsize -1;
-    end
-    if Iyvalue <= 0
-        Iyvalue =1;
-    elseif Iyvalue >= Iysize
-        Iyvalue = Iysize -1;
-    end
-    if Izvalue <= 0
-        Izvalue =1;
-    elseif Izvalue >= Izsize
-        Izvalue = Izsize -1;        
-    end
-    
-    if (I(Ixvalue, Iyvalue, Izvalue) == 0), j = j + 1; else j = 0; end
-        
     % Stop if out of boundary, distance to end smaller then a pixel or
     % if we have not moved for 15 itterations
     if((EndPoint(1)<1) || (EndPoint(1)>size(I,1)) || EndPoint(2)<1 || EndPoint(2)>size(I,2) ||...
@@ -148,6 +102,10 @@ while(true)
         ||(Movement<Stepsize))
         ShortestLine = ShortestLine(1:i,:);
         break;  
+    else
+        if (I(ceil(EndPoint(1)), ceil(EndPoint(2)), ceil(EndPoint(3))) == 0)
+            j = j + 1; else j = 0; 
+        end
     end
 
     % Count the number of itterations
@@ -162,7 +120,7 @@ while(true)
     if(DistancetoEnd<Stepsize), 
         i=i+1;  if(i>ifree), ifree=ifree+10000; ShortestLine(ifree,:)=0; end
         % Add (Last) Source point to the shortest line array
-        ShortestLine(i,:)=SourcePoint(:,ind);
+        ShortestLine(i,:) = SourcePoint(:,ind);
         break 
     end
     
@@ -170,8 +128,6 @@ while(true)
     
     % Current point is next Starting Point
     StartPoint = EndPoint;
-    
-    
 end
 
 % Remove unused memory from array
