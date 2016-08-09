62fe33b0ffa13849213152f4d0531f5e4290d2ea
donghaozhang <dzha9516@uni.sydney.edu.au>
2016-08-09 10:52:57 +1000

    remove some redundant files

--- scripts/gotrain.m
+++
@@ -1,7 +0,0 @@
-function gotrain(path2feat, skip, holdout, modelname, method)
-	addpath('../util');
-	parpool
-	[X, Y, ~] = featcollect(path2feat, skip);
-	[obj, eidx] = train(X, Y, fullfile(path2feat, '..', [modelname, '.mat']), holdout, method);
-	fprintf('Feature Index used for classification: \n', eidx);
-end
