7cffdfebeb2943ffc6e0548536a20c30c2788b92
siqi <lsqshr@gmail.com>
2016-08-11 12:44:44 +1000

    fix find v3draw script

--- scripts/findv3draw.sh
+++ scripts/findv3draw.sh
@@ -1,31 +1,31 @@
 #!bin/bash
-# Script to find all .v3draw files in a folder and save to ../AllFeats/1-N.v3draw
+# Script to find all .v3draw files in a folder and save to ../All-V3DRAW/1-N.v3draw
 
 DATAPATH=$1;
 GTPATH=$1;
 
-TARGETFOLDER=$DATAPATH/../AllFeats;
+TARGETFOLDER=$DATAPATH/../All-V3DRAW;
 mkdir $TARGETFOLDER;
 
 arr=( $(find $DATAPATH -name '*.v3draw') );
 
 for((i=0;i<${#arr[@]};i++))
 do
-	filename=${arr[${i}]};
-	echo "Processing $filename"
-	BASEDIR=$(dirname $filename);
+    filename=${arr[${i}]};
+    echo "Processing $filename"
+    BASEDIR=$(dirname $filename);
 
-	swcarr=( $(find $BASEDIR -name '*.swc') );
-	if [ ${#swcarr[@]} -ne 1 ]; then
-		>&2 echo "More than 1 swc found in $BASEDIR";
+    swcarr=( $(find $BASEDIR -name '*.swc') );
+    if [ ${#swcarr[@]} -ne 1 ]; then
+        >&2 echo "More than 1 swc found in $BASEDIR";
         exit 42;
-	else
+    else
         swcfile=${swcarr[0]};
-		echo "Copying $swcfile to $TARGETFOLDER/$i.swc";
-		cp $swcfile $TARGETFOLDER/$i.swc;
-	fi
+        echo "Copying $swcfile to $TARGETFOLDER/$i.swc";
+        cp $swcfile $TARGETFOLDER/$i.swc;
+    fi
 
-	echo "Copying $filename to $TARGETFOLDER/$i.v3draw";
-	cp $filename $TARGETFOLDER/$i.v3draw;
+    echo "Copying $filename to $TARGETFOLDER/$i.v3draw";
+    cp $filename $TARGETFOLDER/$i.v3draw;
 done
 
