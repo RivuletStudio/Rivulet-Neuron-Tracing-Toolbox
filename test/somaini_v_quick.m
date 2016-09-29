b1d50a9f3a9a7cfb11f1a459235f67c50393bc1a
donghaozhang <dzha9516@uni.sydney.edu.au>
2016-09-29 14:14:56 +1000

    Improve the format of fprintf function call

--- test/somaini_v_quick.m
+++ test/somaini_v_quick.m
@@ -7,10 +7,10 @@ function pasted = somaini_v_quick(imgsoma, somathres)
 	maxxyregion = region(maxareaindex);
 	maxxyregionBoundingBox = maxxyregion.BoundingBox;
 	[xwidth, yheight, zdim] = size(region(maxareaindex).FilledImage);
-	fprintf('xwidth: %4.0d, yheight: %4.0d, zdim: %4.0d\n', xwidth, yheight, zdim)
+	fprintf('xwidth: %4.0d, yheight: %4.0d, zdim: %4.0d\n', xwidth, yheight, zdim);
 	xp = maxxyregionBoundingBox(2);
 	yp = maxxyregionBoundingBox(1);
 	zp = maxxyregionBoundingBox(3);
-	fprintf('xp: %d, yp: %d, zp: %d\n', floor(xp), floor(yp), floor(zp))
+	fprintf('xp: %d, yp: %d, zp: %d\n', floor(xp), floor(yp), floor(zp));
 	pasted(floor(xp): floor(xp) + xwidth - 1, floor(yp) : floor(yp) + yheight - 1,  floor(zp) : floor(zp) + zdim - 1) = region(maxareaindex).FilledImage;
 end
